11a6a28826ce1243cc502df93ec011344e2b03d4
Mauro <mauro@localhost.localdomain>
2017-02-12 17:44:38 -0300

    adds navbar to site, no functionality has been added yet

---
+++ src/js/components/navbarInstance.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import Nav from 'react-bootstrap/lib/Nav';
+import NavItem from 'react-bootstrap/lib/NavItem';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+
+
+const navbarInstance = () => {
+    return (
+        <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#"><i>Fastube</i></a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+                <Nav>
+                    <NavItem eventKey={1} href="#">
+                        <input type="search" placeholder="Your search ..." />
+                    </NavItem>
+                </Nav>
+                <Nav pullRight>
+                    <NavItem eventKey={1} href="#">Sign in</NavItem>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    );
+}
+
+export default navbarInstance;

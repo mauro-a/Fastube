41307966ad3cc0b55b98e6ec2c7043d139d5243e
mauro89x <mauro.a89x@gmail.com>
2017-03-26 13:32:29 -0300

    Adds filters to search WIP

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,12 @@ import Navbar from './components/navbarInstance';
 import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
 const API_KEY = 'AIzaSyCmHD9ETQ7GQ4TQbQvG6MuD5g7Wb_w7HOE';
 
 class App extends React.Component {
     constructor(props) {
         super(props);
+        this.filterVideosByTerm = this.filterVideosByTerm.bind(this);
         this.state = {
             videos: []
         }
@@ -21,12 +21,24 @@ class App extends React.Component {
         });
     }
     
+    filterVideosByTerm(term, filter) {
+        var self = this;
+
+        let options = {
+            part: filter || 'snippet'
+        };
+        YTSearch({'term': term, 'key': API_KEY, 'params': options }, function(data) {
+            self.setState({
+                videos: data
+            });
+        });
+    }
+
     render() {
         return (
             <div>
-                <SearchBar />
-                <Navbar />
-                <VideoList videos={this.state.videos}/>
+                <SearchBar onSearchPerformed={(term, filter) => this.filterVideosByTerm(term, filter) } />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
--- src/js/components/search_bar.js
+++ src/js/components/search_bar.js
@@ -7,22 +7,49 @@ class SearchBar extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-	        term: ''
+	        term: '',
+	        filterBy: ''
 	    }
-	    this.performSearch = this.performSearch.bind(this);
+	    this.onInputChange = this.onInputChange.bind(this);
+	    this.onFilterChange = this.onFilterChange.bind(this);
     }
-	performSearch(aTerm) {
+
+    onFilterChange(aFilter) {
+    	this.setState({
+    		term: '',
+    		filterBy: aFilter
+    	});
+    }
+
+	onInputChange(aTerm, filter) {
+		console.log('SEARCHING TERM: ', aTerm, 'WITH FILTER: ', filter , 'SET');
 		this.setState({term: aTerm});
-		YTSearch({'term': aTerm, 'key': API_KEY }, function(data) {
-	    	console.log(data)
-		});
+		this.props.onSearchPerformed(aTerm);
+	}
+
+	resetFilters() {
+		this.setState({
+    		term: '',
+    		filterBy: ''
+    	});
+    	this.props.onSearchPerformed('PS4', this.state.filterBy);
 	}
 
 	render() {
 		return (
-			<input type="text" placeholder="Your search" 
-				value={this.state.term}
-				onChange={evt => this.performSearch(evt.target.value)} /> 
+			<form>
+				<input type="text" placeholder="Your search" 
+					value={this.state.term}
+					onChange={evt => this.onInputChange(evt.target.value, this.state.filterBy)} /> 
+				<label htmlFor="searchBy">Search by</label>
+				<select id="searchBy" 
+					 onChange={evt => this.onFilterChange(evt.target.value) }> 
+					<option value="" selected>All</option>
+					<option value="title">Title</option>
+					<option value="description">Description</option>
+				</select>
+				<input type="reset" onClick={() => this.resetFilters()} value="Reset to defaults" />
+			</form>
 		); 
 	}
 }
